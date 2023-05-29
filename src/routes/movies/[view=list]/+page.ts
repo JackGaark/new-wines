997f751524d4543871d3ba8df84545c456261215
Jack Gaark <jack.gaarkeuken@gmail.com>
2023-05-29 16:14:48 -0400

    fxing params errors

--- src/routes/movies/[view=list]/+page.ts
+++ src/routes/movies/[view=list]/+page.ts
@@ -1,10 +1,11 @@
-import * as api from "$lib/api";
-import type { MovieList } from "$lib/types.js";
+import * as api from "$lib/api.js";
 import { views } from "$lib/views.js";
+import type { MovieList } from "$lib/types.js";
 
 export async function load({ params, url, fetch }) {
   const view = views[params.view];
-  const page = url.searchParams("page") ?? "1";
+  const page = url.searchParams.get("page") ?? "1";
+
   const data = (await api.get(fetch, view.endpoint, {
     page,
   })) as MovieList;
@@ -14,6 +15,7 @@ export async function load({ params, url, fetch }) {
     title: view.title,
     endpoint: view.endpoint,
     movies: data.results,
-    next_page: data.page < data.total_pages ? data.page + 1 : null,
+    next_page: data.page! < data.total_pages ? data.page + 1 : null,
+    infinite: true,
   };
 }
